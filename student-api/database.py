be07ac1038e50f68d3911b3a8e715e84cbded6fc
Disha <Disha.a@surya-fintech.com>
2026-08-06 22:06:51 +0530

    added cursor connection logic

--- student-api/database.py
+++ student-api/database.py
@@ -1,6 +1,8 @@
+# database.py
 import oracledb
 import os
 from dotenv import load_dotenv
+from contextlib import contextmanager
 
 load_dotenv()
 
@@ -9,3 +11,16 @@ connection = oracledb.connect(
     password=os.getenv("DB_PASSWORD"),
     dsn=os.getenv("DB_DSN")
 )
+
+@contextmanager
+def get_cursor(commit: bool = False):
+    cursor = connection.cursor()
+    try:
+        yield cursor
+        if commit:
+            connection.commit()
+    except Exception:
+        connection.rollback()
+        raise
+    finally:
+        cursor.close()
