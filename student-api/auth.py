ef4a5d5fe3b36ae3c9d49a080a0d2ffc042eb00a
Disha <Disha.a@surya-fintech.com>
2026-08-07 12:55:44 +0530

    add authentication

---
+++ student-api/auth.py
@@ -0,0 +1,18 @@
+# auth.py
+import os
+from dotenv import load_dotenv
+from fastapi import Security, HTTPException, status
+from fastapi.security import APIKeyHeader
+
+load_dotenv()
+
+API_TOKEN = os.getenv("API_TOKEN")
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+
+def verify_api_key(api_key: str = Security(api_key_header)):
+    if api_key is None or api_key != API_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing API key",
+        )
+    return api_key
