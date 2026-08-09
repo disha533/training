d51116e206ad48e018745a55be97b34c7665761e
Disha <Disha.a@surya-fintech.com>
2026-08-10 03:34:42 +0530

    seperated vector and clickhouse logic

--- student-api/controllers/student_controller.py
+++ student-api/controllers/student_controller.py
@@ -1,41 +1,33 @@
+# controllers/student_controller.py
 
-from fastapi import APIRouter, Depends
-from schemas.schema import Student, StudentUpdate
-from vector_store import search_students_vector   
-from services.service import (
-    add_student,
-    get_all_students,
-    get_student_by_id,
-    update_student,
-    delete_student
-)
-from auth import verify_api_key
 
-router = APIRouter(
-    prefix="/students",
-    dependencies=[Depends(verify_api_key)]   
-)
+from fastapi import APIRouter
+from schemas.schema import Student
+from services import student_service as service
+
+router = APIRouter(prefix="/students")
+
 
 @router.post("/")
 def create_student(student: Student):
-    return add_student(student)
+    return service.add_student(student)
+
 
 @router.get("/")
 def get_students():
-    return get_all_students()
+    return service.get_all_students()
+
 
 @router.get("/{student_id}")
 def get_student(student_id: int):
-    return get_student_by_id(student_id)
+    return service.get_student_by_id(student_id)
 
-@router.patch("/{student_id}")
-def update(student_id: int, student: StudentUpdate):
-    return update_student(student_id, student)
+
+@router.put("/{student_id}")
+def update_student(student_id: int, student: Student):
+    return service.update_student(student_id, student)
+
 
 @router.delete("/{student_id}")
-def delete(student_id: int):
-    return delete_student(student_id)
-
-@router.get("/search/")
-def search(query: str, threshold: float = 0.5):
-    return search_students_vector(query, threshold=threshold)
+def delete_student(student_id: int):
+    return service.delete_student(student_id)
